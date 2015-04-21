88a6be3d1c8ded51f5d4661e041aefe2b2fc7c48
Ramon Delgado <Ramon@Ramons-MacBook-Pro.local>
2015-04-21 16:19:46 +0100

    BRAMA

--- server.js
+++ server.js
@@ -11,13 +11,16 @@ var path = require('path'),
 
 // Authenticate against the environment-provided credentials, if running
 // the app in production (Heroku, effectively)
-if (env === 'production') {
-  if (!username || !password) {
-    console.log('Username or password is not set, exiting.');
-    process.exit(1);
-  }
-  app.use(express.basicAuth(username, password));
-}
+
+// if (env === 'production') {
+//  if (!username || !password) {
+//    console.log('Username or password is not set, exiting.');
+//    process.exit(1);
+//  }
+
+  //app.use(express.basicAuth(username, password));
+  app.use(express.basicAuth('brama', 'bramissima'));
+//}
 
 // Application settings
 app.engine('html', require(__dirname + '/lib/template-engine.js').__express);
