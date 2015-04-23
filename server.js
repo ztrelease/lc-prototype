832f49b609c6bce2dba7f65d8fd19f7fe6b8f8e8
Ramon Delgado <Ramon@Ramons-MacBook-Pro.local>
2015-04-23 12:05:11 +0100

    BRAMA

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var path = require('path'),
 //  }
 
   //app.use(express.basicAuth(username, password));
-  app.use(express.basicAuth('brama', 'brama1'));
+  app.use(express.basicAuth('brama', 'brama2'));
 //}
 
 // Application settings
