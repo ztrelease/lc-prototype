7a0e8559d591e8e919f94f3aebb49235757741b2
Ramon Delgado <ramon.delorenzo@yahoo.com>
2015-08-18 13:00:51 +0100

    BRAMA

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var path = require('path'),
 //  }
 
   //app.use(express.basicAuth(username, password));
-  app.use(express.basicAuth('brama', 'brama8'));
+  app.use(express.basicAuth('brama', 'brama10'));
 //}
 
 // Application settings
